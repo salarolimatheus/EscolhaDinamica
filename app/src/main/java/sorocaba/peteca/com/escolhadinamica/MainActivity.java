5a0549dd3734e92c7ca382bd9757693a627325a1
Matheus Salaroli <mat.s@terra.com.br>
2020-01-09 00:35:18 +0100

    Inicio

--- app/src/main/java/sorocaba/peteca/com/escolhadinamica/MainActivity.java
+++ app/src/main/java/sorocaba/peteca/com/escolhadinamica/MainActivity.java
@@ -2,16 +2,41 @@ package sorocaba.peteca.com.escolhadinamica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.weigan.loopview.LoopView;
+import com.weigan.loopview.OnItemSelectedListener;
+
+import java.util.ArrayList;
 
 import sorocaba.peteca.com.dinamicparam.DinamicParam;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toast toast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    DinamicParam din;
+        final LoopView loopView = (LoopView) findViewById(R.id.loopView);
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            list.add("item " + i);
+        }
+        loopView.setListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int index) {
+                if (toast == null) {
+                    toast = Toast.makeText(MainActivity.this, "item " + index, Toast.LENGTH_SHORT);
+                }
+                toast.setText("item " + index);
+                toast.show();
+            }
+        });
+        loopView.setItems(list);
+    }
 }
